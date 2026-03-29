c8b4a407d669acb94d537a2b496dc85d2c58863c
michaelbowser <michael@michaelbowser.org>
2026-03-29 08:15:41 -0500

    added chp 5 work

---
+++ tc_gh_files/nb/.ipynb_checkpoints/LifeRabbits-checkpoint.py
@@ -0,0 +1,34 @@
+""" Code example from Complexity and Computation, a book about
+exploring complexity science with Python.  Available free from
+
+http://greenteapress.com/complexity
+
+Copyright 2016 Allen Downey
+MIT License: http://opensource.org/licenses/MIT
+"""
+
+import sys
+import matplotlib.pyplot as plt
+
+from Life import Life
+
+
+def main(script, *args):
+    """Constructs the rabbits methusela.
+
+    http://www.argentum.freeserve.co.uk/lex_r.htm#rabbits
+    """
+
+    rabbits = ["1000111", "111001", "01"]
+
+    n = 400
+    m = 600
+    life = Life(n, m)
+    life.add_cells(n // 2, m // 2, *rabbits)
+    life.animate(frames=1000)
+    plt.subplots_adjust(left=0.01, right=0.99, bottom=0.01, top=0.99)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main(*sys.argv)
